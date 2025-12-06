8c788d78d9201fcf56f5f3a577eee03adedb76ed
backupjailbreak <4804944-backupjailbreak@users.noreply.replit.com>
2025-12-06 23:10:30 +0000

    Update reboot system

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -2,25 +2,24 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-	name: "reboot",
-	category: "owner",
+		name: "reboot",
+		category: "owner",
 
-	run: async (client, message) => {
-		if (message.author.id !== "704331555853697074") {
-			return message.channel.send("❌ You cannot use this command!");
+		run: async (client, message) => {
+				if (message.author.id !== "704331555853697074") {
+						return message.channel.send("❌ You cannot use this command!");
+				}
+
+				await message.channel.send("🔄 Rebooting bot...");
+
+				const data = { channelId: message.channel.id };
+
+				fs.writeFileSync(
+						path.join(__dirname, "../../utils/reboot.json"),
+						JSON.stringify(data, null, 2),
+						"utf8"
+				);
+
+				setTimeout(() => process.exit(0), 500);
 		}
-
-		await message.channel.send("🔄 Rebooting bot...");
-
-		// Store channelId in correct format used by recovery system
-		const data = { channelId: message.channel.id };
-
-		fs.writeFileSync(
-			path.join(__dirname, "../../utils/reboot.json"),
-			JSON.stringify(data, null, 2),
-			"utf8"
-		);
-
-		setTimeout(() => process.exit(0), 500);
-	}
 };
