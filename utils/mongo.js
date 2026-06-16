0ee732d0b2c802596d297d974d32e542fa6e430b
backupjailbreak <4804944-backupjailbreak@users.noreply.replit.com>
2026-06-16 04:46:40 +0000

    dsfd

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
+  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
+  if (!process.env.MONGO_URI) {
+    console.error("❌ MONGO_URI is missing");
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
