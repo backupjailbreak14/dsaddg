38302fe52e6e27144f0bffff25483874850e7f63
backupjailbreak <4804944-backupjailbreak@users.noreply.replit.com>
2026-07-19 23:18:19 +0000

    dfdsf

--- utils/awardEmojis.js
+++ utils/awardEmojis.js
@@ -6,38 +6,38 @@ module.exports = {
     "Large Marshal's Star": "<:Large_Marshals_Star:1519038567052214545>",
     "Small Marshal's Star": "<:Small_Marshals_Star:1519038617987846174>",
 
-    "Order of the Patriotic War 1st Class": "<:Order_of_Patriotic_War_1st_class:1519568993344815124>",
-    "Order of the Patriotic War 2nd Class": "<:Order_of_Patriotic_War_2nd_class:1519569130846421132>",
+    "Order of the Patriotic War I Class": "<:Order_of_Patriotic_War_1st_class:1519568993344815124>",
+    "Order of the Patriotic War II Class": "<:Order_of_Patriotic_War_2nd_class:1519569130846421132>",
 
     "Order of Alexander Nevsky": "<:Order_of_Alexander_Nevsky:1519564199800344627>",
     "Order of Badge of Honour": "<:Order_of_Badge_of_Honour:1519036961866776646>",
 
-    "Order of Bogdan Khmelnytsky 1st Class": "<:Order_of_Bogdan_Khmelnitskiy:1519042584666705920>",
-    "Order of Bogdan Khmelnytsky 2nd Class": "<:Orderof_Bogdan_Khmelnytsky_2nd:1528291033371119726>",
-    "Order of Bogdan Khmelnytsky 3rd Class": "<:Orderof_Bogdan_Khmelnytsky_3rd:1528291091156045904>",
+    "Order of Bogdan Khmelnytsky I Class": "<:Order_of_Bogdan_Khmelnitskiy:1519042584666705920>",
+    "Order of Bogdan Khmelnytsky II Class": "<:Orderof_Bogdan_Khmelnytsky_2nd:1528291033371119726>",
+    "Order of Bogdan Khmelnytsky III Class": "<:Orderof_Bogdan_Khmelnytsky_3rd:1528291091156045904>",
 
-    "Order of Glory 1st Class": "<:Order_of_Glory_1st_class:1519566822582521959>",
-    "Order of Glory 2nd Class": "<:Order_of_Glory_2nd_class:1519566796720574485>",
-    "Order of Glory 3rd Class": "<:Order_of_Glory_3rd_class:1519566771391041566>",
+    "Order of Glory I Class": "<:Order_of_Glory_1st_class:1519566822582521959>",
+    "Order of Glory II Class": "<:Order_of_Glory_2nd_class:1519566796720574485>",
+    "Order of Glory III Class": "<:Order_of_Glory_3rd_class:1519566771391041566>",
 
-    "Order of Kutuzov 1st Class": "<:Order_of_Kutuzov_1st_class:1519564762143260823>",
-    "Order of Kutuzov 2nd Class": "<:Order_of_Kutuzov_2nd_class:1519564792635985980>",
-    "Order of Kutuzov 3rd Class": "<:Order_of_Kutuzov_3rd_class:1519564816820473987>",
+    "Order of Kutuzov I Class": "<:Order_of_Kutuzov_1st_class:1519564762143260823>",
+    "Order of Kutuzov II Class": "<:Order_of_Kutuzov_2nd_class:1519564792635985980>",
+    "Order of Kutuzov III Class": "<:Order_of_Kutuzov_3rd_class:1519564816820473987>",
 
     "Order of Lenin": "<:Order_of_Lenin:1519566143889739907>",
 
-    "Order of Nakhimov 1st Class": "<:Order_of_Nakhimov_1st_classc:1519575826197381200>",
-    "Order of Nakhimov 2nd Class": "<:Order_of_Nakhimov_2nd_class:1519575848322338936>",
+    "Order of Nakhimov I Class": "<:Order_of_Nakhimov_1st_classc:1519575826197381200>",
+    "Order of Nakhimov II Class": "<:Order_of_Nakhimov_2nd_class:1519575848322338936>",
 
     "Order of the Red Banner": "<:Order_of_the_Red_Banner:1519573679585886349>",
     "Order of the Red Banner of Labour": "<:Order_of_Red_Banner_of_Labour:1519567822244810852>",
 
-    "Order of Suvorov 1st Class": "<:Order_of_Suvorov_1st_class:1519572684386730094>",
-    "Order of Suvorov 2nd Class": "<:Order_of_Suvorov_2nd_class:1519572705182224544>",
-    "Order of Suvorov 3rd Class": "<:Order_of_Suvorov_3rd_class:1528288676440969246>",
+    "Order of Suvorov I Class": "<:Order_of_Suvorov_1st_class:1519572684386730094>",
+    "Order of Suvorov II Class": "<:Order_of_Suvorov_2nd_class:1519572705182224544>",
+    "Order of Suvorov III Class": "<:Order_of_Suvorov_3rd_class:1528288676440969246>",
 
-    "Order of Ushakov 1st Class": "<:Order_of_Ushakov:1519040903258308708>",
-    "Order of Ushakov 2nd Class": "<:Order_of_Ushakov_2nd_class:1528290268468478013>",
+    "Order of Ushakov I Class": "<:Order_of_Ushakov:1519040903258308708>",
+    "Order of Ushakov II Class": "<:Order_of_Ushakov_2nd_class:1528290268468478013>",
 
     "Order of Victory": "<:Order_of_Victory:1519559983174389901>",
     "Order of the Red Star": "<:Order_of_the_Red_Star:1270432896137826314>",
@@ -52,9 +52,9 @@ module.exports = {
 
     "Guards Badge": "<:Guards_Badge:1519579655022182460>",
 
-    "Order of Maternal Glory 1st Class": "<:Order_Maternal_Glory_1st_class:1519570643354521650>",
-    "Order of Maternal Glory 2nd Class": "<:Order_Maternal_Glory_2nd_class:1519570667140288627>",
-    "Order of Maternal Glory 3rd Class": "<:Order_Maternal_Glory_3rd_Class:1519570688997064734>",
+    "Order of Maternal Glory I Class": "<:Order_Maternal_Glory_1st_class:1519570643354521650>",
+    "Order of Maternal Glory II Class": "<:Order_Maternal_Glory_2nd_class:1519570667140288627>",
+    "Order of Maternal Glory III Class": "<:Order_Maternal_Glory_3rd_Class:1519570688997064734>",
 
     "Mother Heroine": "<:Mother_Heroine:1519365043953733743>"
     
