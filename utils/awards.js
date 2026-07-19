d136f7b91bffbbd64648b120f27c50d5d99ef215
backupjailbreak <4804944-backupjailbreak@users.noreply.replit.com>
2026-07-19 23:28:47 +0000

    sdads

--- utils/awards.js
+++ utils/awards.js
@@ -1,75 +1,98 @@
 module.exports = {
 
-// High-level titles (highest prestige categories)
-titles: [
-    "Large Marshal's Star",
-    "Small Marshal's Star",
-    "Hero of the Soviet Union",
-    "Hero of Socialist Labour",
-    "People's Artist of the USSR",
-    "State Stalin Prize"
-],
+    // High-level titles
+    titles: [
+        "Large Marshal's Star",
+        "Small Marshal's Star",
+        "Hero of the Soviet Union",
+        "Hero of Socialist Labour",
+        "People's Artist of the USSR",
+        "State Stalin Prize",
+        "Mother Heroine"
+    ],
 
-// Orders (state honors and military orders)
-orders: [
-    "Order of Victory",
-    "Order of the Red Banner",
-    "Order of the Red Star",
-    "Order of the Patriotic War",
-    "Order of Alexander Nevsky",
-    "Order of Suvorov",
-    "Order of Kutuzov",
-    "Order of Bogdan Khmelnitsky",
-    "Order of Glory",
-    "Order of Lenin",
-    "Order of the Red Banner of Labour",
-    "Order of the Badge of Honour"
-],
+    // Orders
+    orders: [
+        "Order of Victory",
+        "Order of Lenin",
+        "Order of the Red Banner",
+        "Order of the Red Banner of Labour",
+        "Order of the Red Star",
+        "Order of Badge of Honour",
+        "Order of Alexander Nevsky",
 
-// Medals
-medals: [
-    'Medal "For Courage"',
-    'Medal "For Battle Merit"',
-    'Medal "Partisan of the Patriotic War"',
-    'Medal "For the Defence of Leningrad"',
-    'Medal "For the Defence of Odessa"',
-    'Medal "For the Defence of Sevastopol"',
-    'Medal "For the Defence of Stalingrad"',
-    'Medal "For the Defence of Moscow"',
-    'Medal "For the Defence of the Caucasus"',
-    'Medal "For the Defence of the Soviet Transarctic"',
-    'Medal "For the Capture of Berlin"',
-    'Medal "For the Capture of Budapest"',
-    'Medal "For the Capture of Königsberg"',
-    'Medal "For the Capture of Vienna"',
-    'Medal "For the Liberation of Prague"',
-    'Medal "For the Liberation of Warsaw"',
-    'Medal "For the Liberation of Belgrade"',
-    'Medal "For Victory over Germany"',
-    'Medal "For the Victory over Japan"',
-    'Medal "For Labour Valour"',
-    'Medal "For Distinguished Labour"',
-    'Medal "For Valiant Labour in the Great Patriotic War"'
-],
+        "Order of the Patriotic War I Class",
+        "Order of the Patriotic War II Class",
 
-// Badges
-badges: [
-    "Guards Badge",
-    "Excellent Tank Shooting",
-    "Honorable Radio Operator",
-    "Voroshilov Horseman",
-    "Distinguished NKVD Employee",
-    "Excellence in the Red Army",
-    "Excellent WPRA Serviceman",
-    "Voroshilov Highest Military Academy of General Staff Graduate",
-    "CPSU Membership Pin",
-    "Komsomol Membership Pin"
-],
+        "Order of Bogdan Khmelnytsky I Class",
+        "Order of Bogdan Khmelnytsky II Class",
+        "Order of Bogdan Khmelnytsky III Class",
 
-// Not currently used
-ribbons: [],
+        "Order of Glory I Class",
+        "Order of Glory II Class",
+        "Order of Glory III Class",
 
-// Future expansion
-other: []
+        "Order of Kutuzov I Class",
+        "Order of Kutuzov II Class",
+        "Order of Kutuzov III Class",
+
+        "Order of Nakhimov I Class",
+        "Order of Nakhimov II Class",
+
+        "Order of Suvorov I Class",
+        "Order of Suvorov II Class",
+        "Order of Suvorov III Class",
+
+        "Order of Ushakov I Class",
+        "Order of Ushakov II Class",
+
+        "Order of Maternal Glory I Class",
+        "Order of Maternal Glory II Class",
+        "Order of Maternal Glory III Class"
+    ],
+
+    // Medals
+    medals: [
+        'Medal "For Courage"',
+        'Medal "For Battle Merit"',
+        'Medal "Partisan of the Patriotic War"',
+        'Medal "For the Defence of Leningrad"',
+        'Medal "For the Defence of Odessa"',
+        'Medal "For the Defence of Sevastopol"',
+        'Medal "For the Defence of Stalingrad"',
+        'Medal "For the Defence of Moscow"',
+        'Medal "For the Defence of the Caucasus"',
+        'Medal "For the Defence of the Soviet Transarctic"',
+        'Medal "For the Capture of Berlin"',
+        'Medal "For the Capture of Budapest"',
+        'Medal "For the Capture of Königsberg"',
+        'Medal "For the Capture of Vienna"',
+        'Medal "For the Liberation of Prague"',
+        'Medal "For the Liberation of Warsaw"',
+        'Medal "For the Liberation of Belgrade"',
+        'Medal "For Victory over Germany"',
+        'Medal "For the Victory over Japan"',
+        'Medal "For Labour Valour"',
+        'Medal "For Distinguished Labour"',
+        'Medal "For Valiant Labour in the Great Patriotic War"'
+    ],
+
+    // Badges
+    badges: [
+        "Guards Badge",
+        "Excellent Tank Shooting Badge",
+        "Honorable Radio Operator",
+        "Voroshilov Horseman Badge",
+        "Distinguished NKVD Employee",
+        "Excellence in the Red Army",
+        "Excellent WPRA Serviceman",
+        "Voroshilov Highest Military Academy of General Staff Graduate",
+        "CPSU Membership Pin",
+        "Komsomol Membership Pin"
+    ],
+
+    ribbons: [],
+
+    other: []
 
 };
