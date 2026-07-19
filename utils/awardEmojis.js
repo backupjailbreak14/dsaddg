f89cae7cdd6d9a03891bb7e78f5b269c501af669
backupjailbreak <4804944-backupjailbreak@users.noreply.replit.com>
2026-07-19 14:48:04 +0000

    sadasdad

--- utils/awardEmojis.js
+++ utils/awardEmojis.js
@@ -1,47 +1,84 @@
 module.exports = {
 
-    "Hero of Socialist Labour": "<:HOSL:1270433081718870047>",
+    "Hero of Socialist Labour": ":HOSL:",
 
-    "Hero of the Soviet Union": "<:HOTSU:1270433078984314954>",
+    "Hero of the Soviet Union": ":HOTSU:",
 
-    "Large Marshal's Star": "<:Large_Marshals_Star:1519038567052214545>",
+    "Large Marshal's Star": ":Large_Marshals_Star:",
 
-    "Small Marshal's Star": "<:Small_Marshals_Star:1519038617987846174>",
-
-
-    "Order of the Patriotic War": "<:Order_Of_The_Patriotic_War:1270432891322634270>",
-
-    "Order of Alexander Nevsky": "<:Order_of_Alexander_Nevsky:1519042796646830121>",
-
-    "Order of Badge of Honour": "<:Order_of_Badge_of_Honour:1519036961866776646>",
-
-    "Order of Bogdan Khmelnitskiy": "<:Order_of_Bogdan_Khmelnitskiy:1519042584666705920>",
-
-    "Order of Glory": "<:Order_of_Glory:1519044301445926952>",
-
-    "Order of Kutuzov": "<:Order_of_Kutuzov:1519041909627162784>",
-
-    "Order of Lenin": "<:Order_of_Lenin:1270433052765454388>",
-
-    "Order of Nakhimov": "<:Order_of_Nakhimov:1519042321088253994>",
-
-    "Order of the Red Banner": "<:Order_of_Red_Banner:1270433036382638121>",
-
-    "Order of the Red Banner of Labour": "<:Order_of_Red_Banner_of_Labor:1270433026802843690>",
-
-    "Order of Suvorov": "<:Order_of_Suvorov:1519039346437521619>",
-
-    "Order of Ushakov": "<:Order_of_Ushakov:1519040903258308708>",
-
-    "Order of Victory": "<:Order_of_Victory:1270432920506597406>",
-
-    "Order of the October Revolution": "<:Order_of_the_October_Revolution:1270433683819593729>",
-
-    "Order of the Red Star": "<:Order_of_the_Red_Star:1270432896137826314>",
+    "Small Marshal's Star": ":Small_Marshals_Star:",
 
 
-    "People's Artist of the USSR": "<:Peoples_Artist_of_the_USSR:1519038286029652255>",
+    "Order of the Patriotic War 1st Class": ":Order_of_Patriotic_War_1st_class:",
 
-    "State Stalin Prize": "<:State_Stalin_Prize:1519037414390366358>"
+    "Order of the Patriotic War 2nd Class": ":Order_of_Patriotic_War_2nd_class:",
+
+    "Order of Alexander Nevsky": ":Order_of_Alexander_Nevsky:",
+
+    "Order of Badge of Honour": ":Order_of_Badge_of_Honour:",
+
+    "Order of Bogdan Khmelnytsky 1st Class": ":Order__of_Bogdan_Khmelnytsky_1st:",
+
+    "Order of Bogdan Khmelnytsky 2nd Class": ":Order__of_Bogdan_Khmelnytsky_2nd:",
+
+    "Order of Bogdan Khmelnytsky 3rd Class": ":Order__of_Bogdan_Khmelnytsky_3rd:",
+
+    "Order of Glory 1st Class": ":Order_of_Glory_1st_class:",
+
+    "Order of Glory 2nd Class": ":Order_of_Glory_2nd_class:",
+
+    "Order of Glory 3rd Class": ":Order_of_Glory_3rd_class:",
+
+    "Order of Kutuzov 1st Class": ":Order_of_Kutuzov_1st_class:",
+
+    "Order of Kutuzov 2nd Class": ":Order_of_Kutuzov_2nd_class:",
+
+    "Order of Kutuzov 3rd Class": ":Order_of_Kutuzov_3rd_class:",
+
+    "Order of Lenin": ":Order_of_Lenin:",
+
+    "Order of Nakhimov 2nd Class": ":Order_of_Nakhimov_2nd_class:",
+
+    "Order of Nakhimov 1st Class": ":Order_of_Nakhimov_1st_class:",
+
+    "Order of the Red Banner": ":Order_of_the_Red_Banner:",
+
+    "Order of the Red Banner of Labour": ":Order_of_Red_Banner_of_Labour:",
+
+    "Order of Suvorov 1st Class": ":Order_of_Suvorov_1st_class:",
+
+    "Order of Suvorov 2nd Class": ":Order_of_Suvorov_2nd_class:",
+
+    "Order of Suvorov 3rd Class": ":Order_of_Suvorov_3rd_class:",
+
+    "Order of Ushakov 1st Class": ":Order_of_Ushakov_1st_class:",
+
+    "Order of Ushakov 2nd Class": ":Order_of_Ushakov_2nd_class:",
+
+    "Order of Victory": ":Order_of_Victory:",
+
+    "Order of the Red Star": ":Order_of_the_Red_Star:",
+
+    "People's Artist of the USSR": ":Peoples_Artist_of_the_USSR:",
+
+    "State Stalin Prize": ":State_Stalin_Prize:",
+
+    "Voroshilov Horseman Badge": ":Voroshilov_Horseman:",
+
+    "Excellent Tank Shooting Badge": ":Excellent_Tank_Shooting:",
+
+    "Honorable Radio Operator": ":Honorable_Radio_Operator:",
+
+    "Excellent WPRA Serviceman": ":Excellent_WPRA_Serviceman:",
+
+    "Guards Badge": ":Guards_Badge:",
+
+    "Order of Maternal Glory 1st Class": ":Order_Maternal_Glory_1st_class:",
+
+    "Order of Maternal Glory 2nd Class": ":Order_Maternal_Glory_2nd_class:",
+
+    "Order of Maternal Glory 3rd Class": ":Order_Maternal_Glory_3rd_class:",
+
+    "Mother Heroine": ":Mother_Heroine:"
 
 };
